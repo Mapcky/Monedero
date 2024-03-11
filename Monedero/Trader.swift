dea4ddd1907722de945b89190110176387513fbf
Mapcky <mperano625@gmail.com>
2024-03-11 10:45:58 -0300

    func editando con ifs

--- Monedero/Trader.swift
+++ Monedero/Trader.swift
@@ -11,14 +11,14 @@ import UIKit
 class Trader: UIViewController {
     
     //variables
-  /*  
-    var origin1: Float = 1
-    var origin2: Float = 1
-    var origin3: Float = 1
-    var destiny1: Float = 0
-    var destiny2: Float = 0
-    var destiny3: Float = 0*/
-
+    /*
+     var origin1: Float = 1
+     var origin2: Float = 1
+     var origin3: Float = 1
+     var destiny1: Float = 0
+     var destiny2: Float = 0
+     var destiny3: Float = 0*/
+    
     var country: String?
     var balance: Int?
     
@@ -62,52 +62,108 @@ class Trader: UIViewController {
         imageArrow.image = UIImage(systemName:"arrow.right")
         arrow2.image = UIImage(systemName: "arrow.right")
         arrow3.image = UIImage(systemName: "arrow.right")
-        fOrigin1.text = "1"//String(origin1)
-        fOrigin2.text = "1"//String(origin2)
-        fOrigin3.text = "1"// String(origin3)
+        fOrigin1.text = "1"
+        fOrigin2.text = "1"
+        fOrigin3.text = "1"
         countryView()
         if let myBalance = balance {
             balanceNumber.text = "$ \(String(myBalance))"
         } else {
             balanceNumber.text = "ERROR"
         }
-
+        
         
     }
     
+    
     @IBAction func editando(_ sender: Any) {
-        if let origin6 = fOrigin1.text {
-            let xdd = (Float(origin6) ?? 0) * cotizacion.arsToUsd
-            fDestiny1.text = String(xdd)
-         }
-
+        if let buttonPressed = sender as? UITextField {
+            if buttonPressed.tag == 0 {
+                if let origin6 = fOrigin1.text {
+                    let calculo = (Float(origin6) ?? 0) * cotizacion.arsToUsd
+                    fDestiny1.text = String(calculo)
+                }
+            }else if buttonPressed.tag == 1 {
+                if let origin6 = fOrigin2.text {
+                    let calculo = (Float(origin6) ?? 0) * cotizacion.arsToMxn
+                    fDestiny2.text = String(calculo)
+                }
+                
+            }else if buttonPressed.tag == 2 {
+                if let origin6 = fOrigin3.text {
+                    let calculo = (Float(origin6) ?? 0) * cotizacion.arsToSol
+                    fDestiny3.text = String(calculo)
+                }
+                
+            }else if buttonPressed.tag == 3 {
+                if let origin6 = fDestiny1.text {
+                    let calculo = (Float(origin6) ?? 0) * cotizacion.usdToArs
+                    fOrigin1.text = String(calculo)
+                }
+            }else if buttonPressed.tag == 4 {
+                if let origin6 = fDestiny2.text {
+                    let calculo = (Float(origin6) ?? 0) * cotizacion.mxnToArs
+                    fOrigin2.text = String(calculo)
+                }
+            }else if buttonPressed.tag == 5 {
+                if let origin6 = fDestiny3.text {
+                    let calculo = (Float(origin6) ?? 0) * cotizacion.solToArs
+                    fOrigin3.text = String(calculo)
+                }
+            }
+        }
     }
     
     func countryView (){
         switch country {
         case "Argentina": 
             myImage.image = UIImage(named: "Argentina")
+            //text field destiny
             fDestiny1.text = String(cotizacion.arsToUsd)
             fDestiny2.text = String(cotizacion.arsToMxn)
             fDestiny3.text = String(cotizacion.arsToSol)
+            
+            //labels
+            (label1.text, label3.text, label5.text) = ("ARS","ARS","ARS")
+            label2.text = "USD"
+            label4.text = "MXN"
+            label6.text = "SOL"
             break
         case "USA":
             myImage.image = UIImage(named: "EEUU")
             fDestiny1.text = String(cotizacion.usdToArs)
             fDestiny2.text = String(cotizacion.usdToMxn)
             fDestiny3.text = String(cotizacion.usdToSol)
+            
+            //labels
+            (label1.text, label3.text, label5.text) = ("USD","USD","USD")
+            label2.text = "ARS"
+            label4.text = "MXN"
+            label6.text = "SOL"
             break
         case "Mexico":
             myImage.image = UIImage(named: "Mexico")
             fDestiny1.text = String(cotizacion.mxnToArs)
             fDestiny2.text = String(cotizacion.mxnToUsd)
             fDestiny3.text = String(cotizacion.mxnToSol)
+            
+            //labels
+            (label1.text, label3.text, label5.text) = ("MXN","MXN","MXN")
+            label2.text = "ARS"
+            label4.text = "USD"
+            label6.text = "SOL"
             break
         case "Peru":
             myImage.image = UIImage(named: "Peru")
             fDestiny1.text = String(cotizacion.solToArs)
             fDestiny2.text = String(cotizacion.solToUsd)
             fDestiny3.text = String(cotizacion.solToMxn)
+            
+            //labels
+            (label1.text, label3.text, label5.text) = ("SOL","SOL","SOL")
+            label2.text = "ARS"
+            label4.text = "USD"
+            label6.text = "MXN"
             break
         default:
             balanceNumber.text = "ERROR"
